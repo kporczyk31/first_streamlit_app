ca9fe4a6e2d765f570bdcbfbbc80343642aaa1a6
kporczyk31 <107478398+kporczyk31@users.noreply.github.com>
2022-06-14 13:51:22 +0200

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
-import pandas
 
 streamlit.title('wszystko gra')
 
@@ -7,6 +6,7 @@ streamlit.header('nagłówek')
 
 streamlit.text('zwykly tekst')
 
+import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
