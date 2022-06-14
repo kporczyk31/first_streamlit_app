9a189208256373b6c5b6531f177579efb7e471b3
kporczyk31 <107478398+kporczyk31@users.noreply.github.com>
2022-06-14 13:50:44 +0200

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,7 +1,15 @@
 import streamlit
+import pandas
 
 streamlit.title('wszystko gra')
 
 streamlit.header('nagłówek')
 
 streamlit.text('zwykly tekst')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page by typing:
+
+streamlit.dataframe(my_fruit_list)
