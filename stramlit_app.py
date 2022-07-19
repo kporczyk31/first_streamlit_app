970a78a7df7e87c3098acedf3ee428ea599af842
kporczyk31 <107478398+kporczyk31@users.noreply.github.com>
2022-07-19 07:50:46 +0200

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,10 +1,10 @@
 import streamlit
 import pandas
+import requests
+
 
 streamlit.title('wszystko gra')
-
 streamlit.header('nagłówek')
-
 streamlit.text('zwykly tekst')
 
 
@@ -12,3 +12,7 @@ streamlit.text('zwykly tekst')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.dataframe(my_fruit_list)
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
